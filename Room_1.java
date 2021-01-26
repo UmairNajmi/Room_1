54eebb25ed06a637001355895fc8ec34baec8b21
UmairNajmi <68281219+UmairNajmi@users.noreply.github.com>
2021-01-26 20:49:31 +0500

    Add files via upload

---
+++ Room_1.java
@@ -0,0 +1,15 @@
+// Room.java
+// displays the floor space of a room 
+class Room
+{
+ public static void main(String[] args)
+    {
+        double length, width, area;
+        length = 15;
+        width = 25;
+        area = length * width; 
+        System.out.println("The floor space is "
+        + area + " square feet\n");
+        
+    }
+}
